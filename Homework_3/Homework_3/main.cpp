0b453f8c64e6c2ca4925cb5a3670d23d0986da3d
chunyou0830 <chunyou0830@gmail.com>
2015-05-20 22:12:52 +0800

    HW3 Finish

---
+++ Homework_3/Homework_3/main.cpp
@@ -0,0 +1,106 @@
+// Finding equivalence classes
+
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <functional>
+#include <algorithm>
+#include <list>
+#include <stack>
+#include <deque>
+#include <stdio.h>
+
+using namespace std;
+
+class ListNode {
+public:
+    int data;
+    ListNode *link;
+    ListNode(int d){
+        data = d;
+        link = 0;
+    }
+};
+
+typedef ListNode *ListNodePtr;
+
+int total=0;
+
+void equivalence(ListNodePtr *seq, bool *out, int n){
+    for(int k=0; k<n; k++){
+        if(out[k]==false){
+            cout << endl << "A new class " << k;
+            out[k] = true;
+            ListNode *x = seq[k];
+            ListNode *top = 0;
+            while(1){
+                while(x){
+                    int j = x->data;
+                    if(out[j]==false){
+                        cout << "," << j;
+                        out[j] = true;
+                        ListNode *y = x->link;
+                        x->link = top;
+                        top = x;
+                        x = y;
+                    }
+                    else{
+                        x= x->link;
+                    }
+                }
+                if(!top){
+                    break;
+                }
+                else{
+                    x=seq[top->data];
+                    top = top->link;
+                }
+            }
+            total++;
+        }
+    }
+}
+
+int main()
+{
+        int n;
+
+        // process i=j relation
+        int i, j, k;
+        ifstream in_file("equivalence.data.txt");
+        string line;
+        const char	*c_line;
+        int		line_count = 0;
+        if (!in_file.is_open()){
+            return(0);
+        }
+
+        getline (in_file,line);
+        c_line = line.c_str();
+        sscanf(c_line, "%d\n", &n);
+        ListNodePtr *seq = new ListNodePtr[n];
+        bool *out = new bool[n];
+
+        for(int k=0; k<n; k++){
+            seq[k] = 0;
+            out[k] = false;
+        }
+
+        while ( getline (in_file,line) ){
+            c_line = line.c_str();
+            sscanf(c_line, "(%d, %d)\n", &i, &j);
+            ListNode *x = new ListNode(j);
+            x->link = seq[i];
+            seq[i] = x;
+            ListNode *y = new ListNode(i);
+            y->link = seq[j];
+            seq[j] = y;
+        }
+        in_file.close();
+
+        equivalence(seq, out, n);
+        cout << endl;
+
+        cout << "THERE ARE IN TOTAL " << total << " Equiv.Classes" << endl;
+
+}
